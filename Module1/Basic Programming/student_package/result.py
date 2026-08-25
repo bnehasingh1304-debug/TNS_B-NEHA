32a89b8687253fd4addcec0f9e427aed997810ce
B Neha <bnehasingh1304@gmail.com>
2026-08-25 11:37:52 +0530

    Update result.py

--- Module1/Basic Programming/student_package/result.py
+++ Module1/Basic Programming/student_package/result.py
@@ -1,1 +1,13 @@
+def calculate_total(marks1, marks2, marks3):
+    return marks1 + marks2 + marks3
 
+
+def calculate_average(total):
+    return total / 3
+
+
+def check_result(average):
+    if average >= 40:
+        return "Pass"
+    else:
+        return "Fail"
